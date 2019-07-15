e92b0faa50731ba93677fdde49f54227dbf36f5f
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-07-15 14:05:20 -0500

    adding the exciting words

--- scripts/overly-excited.js
+++ scripts/overly-excited.js
@@ -1,25 +1,53 @@
-// Create an array that contains the words in the sentence
+// // Create an array that contains the words in the sentence
+// let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+// /*
+//     The addExcitement function should be an impure function, and accept
+//     the array as the sole argument. It should iterate over the array
+//     and output the words to the browser console.
+// */
+// function addExcitement (theWordArray) {
+
+//     // Each time the for loop executes, you're going to add one more word to this string
+//     let buildMeUp = ""
+
+//     for (let i = 0; i < theWordArray.length; i++) {
+//         buildMeUp = buildMeUp + " " + sentence[i];
+//         console.log(buildMeUp)
+//         // Concatenate the new word onto buildMeUp
+//         // Print buildMeUp to the console
+//     }
+
+// }
+// // Invoke the function and pass in the array
+// addExcitement(sentence)
+
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
 
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
 function addExcitement (theWordArray) {
-
-    // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
 
-    for (let i = 0; i < theWordArray.length; i++) {
+    for (let i = 1; i < theWordArray.length; i++) {
+        if (i % 3 === 0){
+            buildMeUp = buildMeUp + "!"
+        } 
+        else {
         buildMeUp = buildMeUp + " " + sentence[i];
         console.log(buildMeUp)
-        // Concatenate the new word onto buildMeUp
+        }
+        /*
+            If the current word's place (not index) in the array
+            is evenly divisible by 3, add an exclamation point
+            to the end of the word and then concatenate it to
+            `buildMeUp`.
+
+            Otherwise, just concatenate the word itself.
+         */
+
 
         // Print buildMeUp to the console
     }
 
 }
 
-// Invoke the function and pass in the array
 addExcitement(sentence)
